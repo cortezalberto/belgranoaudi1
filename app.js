0ee90c936df2fb018f4cb967a527303570058462
cortezalberto <cortezalberto@gmail.com>
2021-04-06 21:41:42 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log(" te muestro el mensaje")
+console.log(" te muestro otro mensaje")
+console.log(" lo monitorea nodemom")
+console.log(" lo monitorea nodemom 2 el regreso")
+console.log(" un camnbio")
